0809ca6d6f01bfb7b35883dc2bacd37ed00a1413
Guoyinin <1393605153@qq.com>
2023-10-29 11:34:38 +0800

    bsp_usart

--- BSP/bsp_usart/bsp_usart.c
+++ BSP/bsp_usart/bsp_usart.c
@@ -1,8 +1,8 @@
 /**
   ******************************************************************************
-  * @file     文件名.c
-  * @author   作者名
-  * @version  版本号
+  * @file     bsp_usart.c
+  * @author   Guoyinin
+  * @version  v1.0
   * @date     完成时间
   * @brief    简单介绍
   ******************************************************************************
@@ -14,23 +14,98 @@
   ******************************************************************************
   */ 
 /* Includes -------------------------------------------------------------------*/
-#include "xxx.h"
+#include "bsp_usart.h"
+#include "string.h"
+#include "stdlib.h"
 /* Private  typedef -----------------------------------------------------------*/
 /* Private  define ------------------------------------------------------------*/
 /* Private  macro -------------------------------------------------------------*/
 /* Private  variables ---------------------------------------------------------*/
 
+static uint8_t idx=0;
+static USARTInstance *usart_instance[DEVICE_USART_CNT] = {NULL};
 /* Extern   variables ---------------------------------------------------------*/
 /* Extern   function prototypes -----------------------------------------------*/
 /* Private  function prototypes -----------------------------------------------*/
 /* Private  functions ---------------------------------------------------------*/
 
 /**
-  * @brief  
-  * @param 
-  * @param 
-  * @retval 
+  * @brief  注册一个usart实例
   */
+USARTInstance *USART_Register(USART_Init_Config_s *init_config)
+{
+    if(idx > DEVICE_USART_CNT)//定义实例过多
+    {
+      return NULL;
+    }
 
-                
+    USARTInstance *instance = (USARTInstance *)malloc(sizeof(USARTInstance));
+    memset(instance, 0, sizeof(USARTInstance));
+
+    instance->usart_handle = init_config->usart_handle;
+    instance->recv_buff_size = init_config->recv_buff_size;
+    instance->usart_module_callback = init_config->usart_module_callback;
+
+    usart_instance[idx++]=instance;
+
+    return instance;
+}
+/**
+  * @brief  串口发送
+  */
+void USART_Send(USARTInstance *_instance, uint8_t *send_buf, uint16_t send_size, USART_TRANSFER_MODE mode)
+{
+    switch (mode)
+    {
+    case USART_TRANSFER_BLOCKING:
+        HAL_UART_Transmit(_instance->usart_handle, send_buf, send_size,100);
+        break;
+    case USART_TRANSFER_IT:
+        HAL_UART_Transmit_IT(_instance->usart_handle, send_buf, send_size);
+        break;
+    case USART_TRANSFER_DMA:
+        HAL_UART_Transmit_DMA(_instance->usart_handle, send_buf, send_size);
+        break;
+    default:
+        break;
+    }
+}
+
+/**
+  * @brief  串口接收
+  */
+void USART_Receive(USARTInstance *_instance, uint8_t *receive_buf, uint16_t receive_size, USART_TRANSFER_MODE mode)
+{
+    switch (mode)
+    {
+    case USART_TRANSFER_BLOCKING:
+        HAL_UART_Receive(_instance->usart_handle, receive_buf, receive_size,100);
+        break;
+    case USART_TRANSFER_IT:
+        HAL_UART_Receive_IT(_instance->usart_handle, receive_buf, receive_size);
+        break;
+    case USART_TRANSFER_DMA:
+        HAL_UART_Receive_DMA(_instance->usart_handle, receive_buf, receive_size);
+        break;
+    default:
+        break;
+    }
+}
+
+/**
+  * @brief  串口接收中断
+  */
+ void HAL_UART_RxCpltCallback(UART_HandleTypeDef *huart)
+ {  
+    USARTInstance *usart;
+    for(uint8_t i=0;i<idx;i++)
+    {
+      usart=usart_instance[i];
+      if(usart->usart_handle == huart && usart->usart_module_callback != NULL)
+      {
+        usart->usart_module_callback(usart);
+        return ;
+      }
+    }
+ }
 /************************ (C) COPYRIGHT 2023 EPOCH *****END OF FILE****/
--- BSP/bsp_usart/bsp_usart.h
+++ BSP/bsp_usart/bsp_usart.h
@@ -12,16 +12,52 @@
   */ 
 
 /* Define to prevent recursive inclusion -------------------------------------*/
-#ifndef __xxx_h
-#define __xxx_h
+#ifndef __BSP_USART_h
+#define __BSP_USART_h
 
 /* Includes ------------------------------------------------------------------*/
 #include "stdint.h"
-/* Exported types ------------------------------------------------------------*/
-/* Exported constants --------------------------------------------------------*/
+#include "usart.h"
 /* Exported macro ------------------------------------------------------------*/
+
+#define USART_RXBUFF_LIMIT 256 //缓冲区最大限制
+#define DEVICE_USART_CNT 9     // A板至多分配9个串口
+/* Exported types ------------------------------------------------------------*/
+/*usart实例*/
+typedef struct tmp_usart
+{
+    uint8_t recv_buff[USART_RXBUFF_LIMIT]; // 预先定义的最大buff大小,如果太小请修改USART_RXBUFF_LIMIT
+    uint8_t recv_buff_size;                // 模块接收一包数据的大小
+    UART_HandleTypeDef *usart_handle;      // 实例对应的串口句柄
+    void (*usart_module_callback)(struct tmp_usart *);       // 解析收到的数据的回调函数
+    void *id;                              //区分不同的串口
+} USARTInstance;
+
+/* 发送、接收模式枚举 */
+typedef enum
+{
+    USART_TRANSFER_NONE=0,
+    USART_TRANSFER_BLOCKING,
+    USART_TRANSFER_IT,
+    USART_TRANSFER_DMA,
+} USART_TRANSFER_MODE;
+
+/* usart 初始化配置结构体 */
+typedef struct
+{
+    uint8_t recv_buff_size;                // 模块接收一包数据的大小
+    UART_HandleTypeDef *usart_handle;      // 实例对应的usart_handle
+    void (*usart_module_callback)(USARTInstance *);       // 解析收到的数据的回调函数
+    void *id;                              //区分不同的串口
+} USART_Init_Config_s;
+/* Exported constants --------------------------------------------------------*/
 /* Exported functions ------------------------------------------------------- */
 
+void USART_Send(USARTInstance *_instance, uint8_t *send_buf, uint16_t send_size, USART_TRANSFER_MODE mode);
+
+void USART_Receive(USARTInstance *_instance, uint8_t *receive_buf, uint16_t receive_size, USART_TRANSFER_MODE mode);
+
+USARTInstance *USART_Register(USART_Init_Config_s *init_config);
 #endif
 
 /****************** (C) COPYRIGHT 2023 EPOCH *****END OF FILE*************/
